ccdb9a7f14a7f9a6cabdea0dd9297ca89f760a05
shikshya20 <shikshyaadhikari83@gmail.com>
2025-04-23 12:32:36 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const body=document.getElementsByTagName("body")[0]
+function setColor(name){
+    body.style.backgroundColor=name;
+}
+function randomColor(){ //creating random rgb values
+    const red=Math.round(Math.random()*255)
+    const green=Math.round(Math.random()*255)
+    const blue=Math.round(Math.random()*255)
+    const color =`rgb(${red},${green},${blue})`
+body.style.backgroundColor=color;
+}
+randomColor()
